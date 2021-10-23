b69c48b3664579ae82dafe614333f0f7abb1a64f
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2021-10-23 13:40:49 -0700

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,71 +1,27 @@
 // ==UserScript==
-// @name         Agar.io clone bots! And .io Bots 2021!
-// @namespace    Bots
-// @version      v.18
-// @description  The best bots for agario clones & .io games!
+// @name         Cellsbox.io Bots
+// @namespace    discord.gg/bAstbAfem9
+// @version      1.0.0
+// @description  Bots for cellsbox.io. DM me on Discord for more info.
 // @author       Tatsuya
-// @match        *.powerline.io/*
-// @match        *.es.agar.live/*
-// @match        *.aquar.io/*
-// @match        *.oceanar.io/*
-// @match        *.agarabi.net/*
-// @match        *.agarblack.com/*
-// @match        *.tr.agar.live/*
-// @match        *.agar-io.live/*
-// @match        *.agars.live/*
-// @match        *.agarr.live/*
-// @match        *.agar.live/*
-// @match        *.www.inciagario.net/*
-// @match        *.agariott.com/*
-// @match        *.agario.zafer2.com/*
-// @match        *.agario.work/*
-// @match        *.agario.network/*
-// @match        *.agar.rip/*
-// @match        *.agar.chat/*
-// @match        *.agario.nl/*
-// @match        *.agariomoddedserver.com/*
-// @match        *.agario.in/*
-// @match        *.agario.id/*
-// @match        *.agarplay.club/*
-// @match        *.agariomodded.com/*
-// @match        *.bestagario.org/*
-// @match        *.agarprivateserver.com/*
-// @match        *.agariohub.cc/*
-// @match        *.agarr.live/*
-// @match        *.agarprivateservers.org/*
-// @match        *.agar.cc/*
-// @match        *.agario.one/*
-// @match        *.agar.team/*
-// @match        *.agar.club/*
-// @match        *.agarprivateservers.net/*
-// @match        *.agario.tube/*
-// @match        *.agarprivateserver.com/*
-// @match        *.agario.cc/*
-// @match        *.easyagario.icu/*
-// @match        *.agario.red/*
+// @match        *.cellsbox.io/*
+// @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNVczs2oU6qdgJBw2ZSSe4ibVAGjaZMgWosjYzjXZU1B6Lp9MHoQ27ARzAtofWYHxz3U&usqp=CAU
 // @run-at       document-end
-// @icon         https://static.wikia.nocookie.net/jacksepticeye/images/2/2e/Agar.io_logo.png/revision/latest?cb=20190228055524
 // @grant        none
 // ==/UserScript==
 
-//// Controls:
-// E - Split bots.
-// R - Make bots Eject mass.
-// M - Spam chat[DISABLED].
+// Controls:
+// Q - Split bots.
+// W - Eject mass from bots.
 ////
 
 (function(weapon, userScript) {
-
     console.log("Bots Loaded!");
-    window.pkgfs = 18159535;
-    window.userScriptVersion = userScript.version;
     weapon.script.src = weapon.scriptURL;
     weapon.node.appendChild(weapon.script);
 
 })({
     script: document.createElement("script"),
-    scriptURL: "https://paste.in/raw/Jao31m",
+    scriptURL: "https://paste.in/raw/fjV8Kl",
     node: document.getElementsByTagName("html")[0] || document.getElementsByTagName("body")[0]
-}, {
-    version: GM_info.script.version
 });
