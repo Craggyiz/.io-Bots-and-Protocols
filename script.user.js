db3afbc7395f2e5f4762c7a8d224741a8ac43f18
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2021-10-30 11:06:31 -0700

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,27 +1,20 @@
 // ==UserScript==
-// @name         Cellsbox.io Bots
+// @name         Powerline.io Bots!
 // @namespace    discord.gg/bAstbAfem9
-// @version      1.0.0
-// @description  Bots for cellsbox.io. DM me on Discord for more info.
+// @version      1.0
+// @description  Bots for powerline.io.
 // @author       Tatsuya
-// @match        *.cellsbox.io/*
+// @match        *.powerline.io/*
 // @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNVczs2oU6qdgJBw2ZSSe4ibVAGjaZMgWosjYzjXZU1B6Lp9MHoQ27ARzAtofWYHxz3U&usqp=CAU
-// @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
-// Controls:
-// Q - Split bots.
-// W - Eject mass from bots.
-////
-
 (function(weapon, userScript) {
-    console.log("Bots Loaded!");
     weapon.script.src = weapon.scriptURL;
     weapon.node.appendChild(weapon.script);
 
 })({
     script: document.createElement("script"),
-    scriptURL: "https://paste.in/raw/fjV8Kl",
+    scriptURL: "https://paste.in/raw/hIlpyI",
     node: document.getElementsByTagName("html")[0] || document.getElementsByTagName("body")[0]
 });
