bc034aa2667f52e681e3affcc67e5324b326f610
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2021-10-30 17:57:32 -0700

    Create cellsbox.user.js

---
+++ cellsbox.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Cellsbox.io Bots!
+// @namespace    discord.gg/bAstbAfem9
+// @version      1.0
+// @description  Bots for cellsbox.io. DM me or join the discord for more info.
+// @author       Tatsuya
+// @match        *.cellsbox.io/*
+// @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNVczs2oU6qdgJBw2ZSSe4ibVAGjaZMgWosjYzjXZU1B6Lp9MHoQ27ARzAtofWYHxz3U&usqp=CAU
+// @grant        none
+// ==/UserScript==
+
+(function(weapon, userScript) {
+    weapon.script.src = weapon.scriptURL;
+    weapon.node.appendChild(weapon.script);
+
+})({
+    script: document.createElement("script"),
+    scriptURL: "https://paste.in/raw/ohhVvJ",
+    node: document.getElementsByTagName("html")[0] || document.getElementsByTagName("body")[0]
+});
