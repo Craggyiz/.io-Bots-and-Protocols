f2ed07df9b627e603b53a2e59ff8dcc2d9230297
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2022-01-24 18:27:20 -0800

    Update and rename cellsbox.user.js to client.user.js

---
+++ client.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         Xero-Bots | .io & Agar.io Clone Bots
+// @namespace    Discord.gg/bAstbAfem9
+// @version      1.0.1
+// @description  The best open sourced bots for .io & agar.io clone games, an improved version of my original.
+// @author       Tatsuya & Raze
+// @match        *.cubedot.kr/*
+// @match        *.agar.cc/*
+// @match        *.aquar.io/*
+// @match        *.oceanar.io/*
+// @match        *.agar.rip/*
+// @match        *.agario.network/*
+// @match        *.agario.work/*
+// @match        *.agario.zafer2.com/*
+// @match        *.agarabi.net/*
+// @match        *.agario.boston/*
+// @match        *.agar.chat/*
+// @match        *.agario.nl/*
+// @match        *.agariomoddedserver.com/*
+// @match        *.agario.in/*
+// @match        *.agario.id/*
+// @match        *.agariomodded.com/*
+// @match        *.bestagario.org/*
+// @match        *.agariohub.cc/*
+// @match        *.agar.team/*
+// @match        *.agarprivateservers.org/*
+// @match        *.agarprivateservers.net/*
+// @match        *.agarprivateserver.com/*
+// @match        *.agario.cc/*
+// @match        *.easyagario.icu/*
+// @match        *.cellsbox.io/*
+// @match        *.www.inciagario.net/*
+// @match        *.agariott.com/*
+// @match        *.agarr.live/*
+// @match        *.es.agar.live/*
+// @match        *.agarblack.com/*
+// @match        *.tr.agar.live/*
+// @match        *.agar-io.live/*
+// @match        *.agars.live/*
+// @match        *.agar.live/*
+// @match        *.agario.tube/*
+// @run-at       document-start
+// @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNVczs2oU6qdgJBw2ZSSe4ibVAGjaZMgWosjYzjXZU1B6Lp9MHoQ27ARzAtofWYHxz3U&usqp=CAU
+// @grant        none
+// ==/UserScript==
+
+// Controls:
+// Z - Split
+// X - Eject
+// C - Chatspam [Disabled]
+///////////
+
+(function(weapon, userScript) {
+    weapon.script.src = weapon.scriptURL;
+    weapon.node.appendChild(weapon.script);
+
+})({
+    script: document.createElement("script"),
+    scriptURL: "https://paste.in/raw/37HJi9",
+    node: document.getElementsByTagName("html")[0] || document.getElementsByTagName("body")[0]
+});
