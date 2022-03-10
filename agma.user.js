5a0df64cc698ed228e21e7b24f61b9eb07e5677a
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2022-03-09 18:37:03 -0800

    Update agma.user.js

--- agma.user.js
+++ agma.user.js
@@ -1,1 +1,12 @@
-
+// ==UserScript==
+// @name         Xero-Bots
+// @namespace    https://discord.com/invite/bAstbAfem9
+// @version      1.0.2
+// @description  Best bots for popular agar.io clone games.
+// @author       Tatsuya & Enes
+// @match        *.agma.io/*
+// @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNVczs2oU6qdgJBw2ZSSe4ibVAGjaZMgWosjYzjXZU1B6Lp9MHoQ27ARzAtofWYHxz3U&usqp=CAU
+// @require      https://paste.in/raw/qMcQDI
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
