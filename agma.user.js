52af58a240de6397bacf936c31b8d09ff4d86d94
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2022-03-22 19:44:09 -0700

    Update agma.user.js

--- agma.user.js
+++ agma.user.js
@@ -5,6 +5,8 @@
 // @description  Best bots for popular agar.io clone games.
 // @author       Tatsuya & Enes
 // @match        *.agma.io/*
+// @match        *.cellcraft.io/*
+// @match        *.agar.live/*
 // @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNVczs2oU6qdgJBw2ZSSe4ibVAGjaZMgWosjYzjXZU1B6Lp9MHoQ27ARzAtofWYHxz3U&usqp=CAU
 // @require      https://paste.in/raw/qMcQDI
 // @run-at       document-start
