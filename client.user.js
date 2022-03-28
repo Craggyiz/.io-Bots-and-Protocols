aa790203a3c5034d8db7568eaacd5c9d4db4dd4a
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2022-03-27 18:08:50 -0700

    Update client.user.js

--- client.user.js
+++ client.user.js
@@ -7,6 +7,7 @@
 // @match        *.cellsbox.io/*
 // @match        *.oceanar.io/*
 // @match        *.aquar.io/*
+// @match        *.agar.live/*
 // @run-at       document-end
 // @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNVczs2oU6qdgJBw2ZSSe4ibVAGjaZMgWosjYzjXZU1B6Lp9MHoQ27ARzAtofWYHxz3U&usqp=CAU
 // @grant        none
