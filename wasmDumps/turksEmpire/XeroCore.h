6433538321bd875e6d4ea41b37933a153a0f6c58
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2022-10-07 22:26:02 -0700

    Add files via upload

---
+++ wasmDumps/turksEmpire/XeroCore.h
@@ -0,0 +1,49 @@
+#ifndef XEROCORE_H_GENERATED_
+#define XEROCORE_H_GENERATED_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include "wasm-rt.h"
+
+#ifndef WASM_RT_MODULE_PREFIX
+#define WASM_RT_MODULE_PREFIX
+#endif
+
+#define WASM_RT_PASTE_(x, y) x ## y
+#define WASM_RT_PASTE(x, y) WASM_RT_PASTE_(x, y)
+#define WASM_RT_ADD_PREFIX(x) WASM_RT_PASTE(WASM_RT_MODULE_PREFIX, x)
+
+/* TODO(binji): only use stdint.h types in header */
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint32_t u32;
+typedef int32_t s32;
+typedef uint64_t u64;
+typedef int64_t s64;
+typedef float f32;
+typedef double f64;
+
+extern void WASM_RT_ADD_PREFIX(init)(void);
+
+/* export: 'memory' */
+extern wasm_rt_memory_t (*WASM_RT_ADD_PREFIX(Z_memory));
+/* export: 'run_1' */
+extern u32 (*WASM_RT_ADD_PREFIX(Z_run_1Z_ii))(u32);
+/* export: 'run_0' */
+extern u32 (*WASM_RT_ADD_PREFIX(Z_run_0Z_ii))(u32);
+/* export: '_rotr' */
+extern u32 (*WASM_RT_ADD_PREFIX(Z__rotrZ_iiiii))(u32, u32, u32, u32);
+/* export: 'rotate' */
+extern u32 (*WASM_RT_ADD_PREFIX(Z_rotateZ_iiiii))(u32, u32, u32, u32);
+/* export: 'decrypt_key' */
+extern u32 (*WASM_RT_ADD_PREFIX(Z_decrypt_keyZ_iiiiii))(u32, u32, u32, u32, u32);
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* XEROCORE_H_GENERATED_ */
