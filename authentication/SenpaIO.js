0b7ffb50615d85f11f75c82bddda9917bfd8f66b
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2023-08-25 23:30:37 -0700

    Finalize SenpaIO.js

--- authentication/SenpaIO.js
+++ authentication/SenpaIO.js
@@ -8,13 +8,27 @@
  */
 
 /**
+ * Update 08/25/23
+ * I was cleaning some files, and finally decided to release basically all of the rest of the code.
+ * I removed some important parts of the final layer, and shifted some thing around as well as removed some checks.
+ * So people can't just bot the game to death, but it'll take anyone who knows what they're doing less than 5 minutes
+ * to fix the code and get it fully working again as I didn't remove too much. 
+ * But, I doubt the game will get botted even after this.
+ * 
+ * This will most likely be the only open-sourced code of this, as no one else, besides me, has been able to fully bypass
+ * both Tricksplit's and Senpa.io's Anti-Bot.
+ * 
+ * Tricksplit's solve is basically the same as senpa's even arithmetic wise, so this will also apply to Tricksplit.
+ */
+
+/**
  * @author (Bot) systemrobot on Discord
  * 
  * @license MIT
  * 
  * @note
  * Senpa.io's Bot Protection.
- * Documented and open sourced (partially) as of (7/30/2023).
+ * Documented and open sourced (Mostly) as of (7/30/2023).
  * Implemented in the game (URL: https://senpa.io/web/) using a WebAssembly (WASM) file, which can be found at: https://senpa.io/web/build/bundle.wasm
  * 
  * Precisely translated from WASM to JS.
@@ -33,9 +47,13 @@
  * Packet two is generated using xor of previously generated random values one and two as a seed, along with the 4 values from the new Uint32Array conversion.
  * This packet is also auto-sent by the WASM via the binded WebSocket.
  * 
- * Layer 3: Solve Server Message
+ * Layer 3: Solve Server Message (processInstructions)
  * The final layer involves solving an onmessage buffer received from the server and sending the value back of that solved message to the server for validation. 
- * However, this part of the code is not open-sourced for security reasons.
+ * This layer is known as the "Vm" layer due to the fact that it's using a virtual machine like structure to interpret
+ * a custom set of bytecode instuctions. It uses two stacks to perform various operations based on the instruction set provided.
+ * These include arithmetic calculations, bitwise operations, stack manipulations, and string transformations. The function iterates through the instructions array and performs actions based on the value of each instruction, modifying the stacks as it goes along. 
+ * 
+ * Through this the game generates a token, which is a token the user must send to the server to spawn in.
  * 
  */
 
@@ -125,60 +143,233 @@ class SenpaioWasmInstance {
   }
 
   /**
-   * Solves the array based on the pattern.
+   * Main virtual machine solve code.
    * @private
-   * @note This function contains the logic for solving the array based on the pattern. This part is not open-sourced.
+   * @param {Array} instructions - The byteCode to solve.
    */
-  #handleSolve(patternedValue, bytes, results) {
-    // Code for solving the array based on the pattern (not open-sourced).
-  }
+  #processInstructions(instructions) {
+    var mainStackPusher = [];
+    var backupStack = [];
 
-  /**
-   * Processes the input array and performs certain operations based on the pattern.
-   * @private
-   * @param {Uint8Array} array - The input array to process.
-   * @returns {Array} An array of results after processing.
-   * @note This function processes the input array and applies certain operations based on the pattern. The logic for solving the array is not open-sourced.
-   */
-  #processArray(array) {
-    if (!(array instanceof Uint8Array)) {
-      throw new Error("Input must be a Uint8Array.");
-    }
+    for (var index = 0; index < instructions.length;) {
+      var instructionSet = instructions[index++];
 
-    array = Array.from(array);
+      switch (instructionSet) {
+        case 0:
+          break;
 
-    let findCaseSwitch = 0;
-    let bytes = [];
-    const results = [];
+        case 2:
+          var popValue = mainStackPusher.pop();
+          var indexValue = backupStack[popValue];
+          mainStackPusher.push(indexValue);
+          break;
 
-    for (let i = 0; i < array.length; i++) {
-      if (i % 5 === 0) {
-        if (bytes.length > 0) {
-          this.#handleSolve(findCaseSwitch, bytes, results);
-          bytes = [];
-        }
-        findCaseSwitch = array[i];
-      } else {
-        bytes.push(array[i]);
+        case 3:
+          var indexValue = mainStackPusher.pop();
+          var newValue = mainStackPusher.pop();
+          backupStack[indexValue] = newValue;
+          break;
+
+        case 4:
+          var lastElementIndex = mainStackPusher.length - 1;
+          var lastElement = mainStackPusher[lastElementIndex];
+          mainStackPusher.push(lastElement);
+          break;
+
+        case 5:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+          mainStackPusher.push(popOne);
+          mainStackPusher.push(popTwo);
+          break;
+
+        case 0x10:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+          if (popOne === 0) {
+            mainStackPusher.push(popOne + popTwo);
+          } else {
+            mainStackPusher.push(0);
+          }
+          break;
+
+        case 0x11:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+          if (popOne === 0) {
+            mainStackPusher.push(popOne - popTwo);
+          } else {
+            mainStackPusher.push(0);
+          }
+          break;
+
+        case 0x12:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+          var result = Math.imul(popTwo, popOne);
+          mainStackPusher.push(result);
+          break;
+
+        case 0x13:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+          if (popOne === 0) {
+            mainStackPusher.push(0);
+          } else {
+            mainStackPusher.push(popTwo / popOne);
+          }
+          break;
+
+        case 0x14:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+          mainStackPusher.push(popOne ^ popTwo);
+          break;
+
+        case 0x15:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+
+          mainStackPusher.push(popTwo << (popOne & 0x1f));
+          break;
+
+        case 0x16:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+
+          mainStackPusher.push(popTwo >> (popOne & 0x1f));
+          break;
+
+        case 0x17:
+          var popOne = mainStackPusher.pop();
+          var popTwo = mainStackPusher.pop();
+          mainStackPusher.push(popOne & popTwo);
+          break;
+
+        case 0x20:
+          var popOne = mainStackPusher.pop();
+          mainStackPusher.push(popOne ^ 0xFFFFFFFF);
+          break;
+
+        case 0x40:
+          var str = mainStackPusher.pop();
+          var strLength = str.length;
+          var newArray = [];
+
+          for (var i = strLength - 1; i >= 0; i--) {
+            var charCode = str.charCodeAt(i);
+            var transformedCode = charCode ^ 0xb2;
+            var newChar = String.fromCharCode(transformedCode);
+            newArray.push(newChar);
+          }
+
+          newArray.push(strLength);
+
+          var newString = newArray.join('');
+
+          mainStackPusher.push(newString);
+          break;
+
+        case 0x41:
+          var loopCount = mainStackPusher.pop();
+          if (loopCount < 1) {
+            loopCount = 0;
+          }
+
+          var newArray = [];
+
+          for (var i = 0; i < loopCount; i++) {
+            var nextInstruction = mainStackPusher.pop();
+            nextInstruction = nextInstruction ^ 0xb2;
+            var newChar = String.fromCharCode(nextInstruction);
+            newArray.push(newChar);
+          }
+
+          var newString = newArray.join('');
+
+          mainStackPusher.push(newString);
+          break;
+
+        case 0x42:
+          var loopCount = instructions[index++];
+          if (loopCount < 1) {
+            loopCount = 0;
+          }
+
+          var newArray = [];
+
+          for (var i = 0; i < loopCount; i++) {
+            var nextInstruction = instructions[index++];
+            nextInstruction = nextInstruction ^ 0xb2;
+            var newChar = String.fromCharCode(nextInstruction);
+            newArray.push(newChar);
+          }
+
+          var newString = newArray.join('');
+
+          mainStackPusher.push(newString);
+          break;
+
+        case 0x43:
+          var index = mainStackPusher.pop();
+          var array = mainStackPusher.pop();
+          var result = array[index];
+          mainStackPusher.push(result);
+          break;
+
+        case 0x44:
+          var index = mainStackPusher.pop();
+          var array = mainStackPusher.pop();
+          var value = mainStackPusher.pop();
+          array[index] = value;
+          break;
+
+        case 0x30:
+          var getIndex = mainStackPusher.pop();
+          index = getIndex;
+          break;
+
+        case 0x31:
+          var popOne = mainStackPusher.pop() | 0;
+          var popTwo = mainStackPusher.pop();
+          if (popTwo == 0) {
+            index = popOne;
+          }
+          break;
+
+        case 0x84:
+          var index1 = instructions[index++];
+          var index2 = instructions[index++];
+          var index3 = instructions[index++];
+          var index4 = instructions[index++];
+
+          mainStackPusher.push(index1 | index2 << 8 | index3 << 0x10 | index4 << 0x18);
+          break;
+
+        case 0x85:
+          // Removed
+          break;
+
+        case 0x60:
+          mainStackPusher.push(window || Window)
+          break;
+
+        default:
+          break;
       }
     }
-
-    if (bytes.length > 0) {
-      this.#handleSolve(findCaseSwitch, bytes, results);
-    }
-
-    return results;
   }
 
   /**
-   * Solves the secret data using a certain algorithm.
+   * Solves the secret data using the virtual machine.
    * @public
    * @param {Uint8Array} data - The secret data to be solved.
-   * @note This function solves the secret data using a certain algorithm. The details of the algorithm are not open-sourced for security reasons.
+   * @note This function solves the secret data using the virtual machine instructions.
    */
   solveSecret(data) {
-    this.secretOut = this.#processArray(data);
-    // Code for solving the secret data using a certain algorithm (not open-sourced).
+    data = Array.from(data);
+    this.secretOut = this.#processInstructions(data);
+    // Rest of secretOut code has been redacted still.
   }
 
   /**
